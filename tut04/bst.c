ddecbd8cfee122e82dfbdfbc778e2d9f79a4cfb3
Franco Reyes <franco.javier.reyes@gmail.com>
2024-09-30 15:48:06 +1000

    Update bst starter code to be lowercase

--- tut04/bst.c
+++ tut04/bst.c
@@ -1,26 +1,26 @@
-// Implementation for BST ADT
+// Implementation for bst ADT
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "BST.h"
-// note: typedef struct node *BST;
+#include "bst.h"
+// note: typedef struct node *Bst;
 
 struct node {
 	int value;
-	struct node *left;
-	struct node *right;
+	Bst left;
+	Bst right;
 };
 
-static struct node *newNode(int value);
+static Bst newNode(int value);
 
 /**
  * Frees the tree
 */
-void BSTFree(BST t) {
+void bstFree(Bst t) {
     if (t != NULL) {
-        BSTFree(t->left);
-        BSTFree(t->right);
+        bstFree(t->left);
+        bstFree(t->right);
         free(t);
     }
 }
@@ -28,22 +28,22 @@ void BSTFree(BST t) {
 /**
  * Insert a value into a tre
 */
-BST BSTInsert(BST t, int value) {
+Bst bstInsert(Bst t, int value) {
 	if (t == NULL) {
 		return newNode(value);
 	}
 	
 	if (value < t->value) {
-		t->left = BSTInsert(t->left, value);
+		t->left = bstInsert(t->left, value);
 	} else if (value > t->value) {
-		t->right = BSTInsert(t->right, value);
+		t->right = bstInsert(t->right, value);
 	}
 
 	return t;
 }
 
-static struct node *newNode(int value) {
-	struct node *n = malloc(sizeof(*n));
+static Bst newNode(int value) {
+	Bst n = malloc(sizeof(*n));
 	if (n == NULL) {
 		fprintf(stderr, "Error: out of memory");
 		exit(EXIT_FAILURE);
@@ -56,9 +56,16 @@ static struct node *newNode(int value) {
 }
 
 /**
+ * Count the total number of nodes in a tree
+ */
+int bstNumNodes(Bst t) {
+    return 0;
+}
+
+/**
  * Counts the number of odd values in a tree
 */
-int BSTCountOdds(BST t) {
+int bstCountOdds(Bst t) {
     return 0;
 }
 
@@ -66,8 +73,18 @@ int BSTCountOdds(BST t) {
  * Count number of internal nodes in a given tree.
  * An internal node is a node with at least one non-empty subtree.
 */
-int BSTCountInternal(BST t) {
+int bstCountInternal(Bst t) {
 	return 0;
+}
+
+/**
+ * Compute the height of a tree. The height of a tree is defined as the
+ * length of the longest path from the root to a leaf. The path length is
+ * a count of the number of links (edges) on the path. The height of an
+ * empty tree is -1.
+ */
+int bstHeight(Bst t) {
+    return -1;
 }
 
 /**
@@ -75,7 +92,7 @@ int BSTCountInternal(BST t) {
  * otherwise the function returns -1 (when a given key is not in the binary search tree).
  * The level of the root node is zero. 
 */
-int BSTNodeLevel(BST t, int key) {
+int bstNodeLevel(Bst t, int key) {
 	return 0;
 }
 
@@ -83,6 +100,6 @@ int BSTNodeLevel(BST t, int key) {
  * Counts the number of values that are greater than a given value.
  * This function should avoid visiting nodes that it doesn't have to visit
 */
-int BSTCountGreater(BST t, int val) {
+int bstCountGreater(Bst t, int val) {
 	return 0;
 }
--- tut04/bst.h
+++ tut04/bst.h
@@ -1,39 +1,50 @@
-// Interface for BST ADT
+// Interface for bst
 
-#define NOT_HEIGHT_BALANCED -99
-
-typedef struct node *BST;
+typedef struct node *Bst;
 
 /**
  * Frees the tree
 */
-void BSTFree(BST t);
+void bstFree(Bst t);
 
 /**
  * Insert a value into a tree
 */
-BST BSTInsert(BST t, int value);
+Bst bstInsert(Bst t, int value);
+
+/**
+ * Count the total number of nodes in a tree
+ */
+int bstNumNodes(Bst t);
 
 /**
  * Counts the number of odd values in a tree
 */
-int BSTCountOdds(BST t);
+int bstCountOdds(Bst t);
 
 /**
  * Count number of internal nodes in a given tree.
  * An internal node is a node with at least one non-empty subtree.
 */
-int BSTCountInternal(BST t);
+int bstCountInternal(Bst t);
+
+/**
+ * Compute the height of a tree. The height of a tree is defined as the
+ * length of the longest path from the root to a leaf. The path length is
+ * a count of the number of links (edges) on the path. The height of an
+ * empty tree is -1.
+ */
+int bstHeight(Bst t);
 
 /**
  * Returns the level of the node containing a given key if such a node exists,
  * otherwise the function returns -1 (when a given key is not in the bst).
  * The level of the root node is zero. 
 */
-int BSTNodeLevel(BST t, int key);
+int bstNodeLevel(Bst t, int key);
 
 /**
  * Counts the number of values that are greater than a given value.
  * This function should avoid visiting nodes that it doesn't have to visit
 */
-int BSTCountGreater(BST t, int val);
+int bstCountGreater(Bst t, int val);
